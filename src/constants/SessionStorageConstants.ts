51bf3d84c68912eee35697e189d4ff62577963e2
Arunkumar Selvaraj <arun@ioak.io>
2022-12-14 15:05:29 +0530

    deployment version

--- src/constants/SessionStorageConstants.ts
+++
@@ -1,4 +0,0 @@
-export const NEURALWEB_PREF_ADDEXPENSE_ANOTHER =
-  'neuralweb_pref_addexpense_another';
-export const NEURALWEB_PREF_ADDBILL_ANOTHER = 'neuralweb_pref_addbill_another';
-export const NEURALWEB_PREF_ADDBILL_DATE = 'neuralweb_pref_addbill_date';
